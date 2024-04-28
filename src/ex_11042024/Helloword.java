0279d7e2bb69bf820e36f436bf676f4cb9ecec74
kapil <kapilroe@gmail.com>
2024-04-28 17:36:26 +0530

    first commit

---
+++ src/ex_11042024/Helloword.java
@@ -0,0 +1,11 @@
+package ex_11042024;
+
+public class Helloword {
+    public static void main(String[] args) {
+System.out.println("Hello World");
+System.out.println(2+2);
+System.out.println(2*2);
+System.out.println(2-2);
+System.out.println(2/2);
+    }
+}
