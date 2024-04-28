dc40bec44ff41db9966d52d1b63a2c92d5e1c59c
kapil <kapilroe@gmail.com>
2024-04-29 01:30:35 +0530

    second commit

---
+++ src/ex_11042024/Lab001_int.java
@@ -0,0 +1,11 @@
+package ex_11042024;
+
+public class Lab001_int {
+    public static void main(String[] args) {
+
+        int a = 23;
+        int b = 44;
+        int number = 9;
+        System.out.printf("Your number is %d, %d, %d", a,b, number);
+    }
+}
---
+++ src/ex_11042024/Lab002_float.java
@@ -0,0 +1,10 @@
+package ex_11042024;
+
+public class Lab002_float {
+    public static void main (String [] args) {
+
+        float pi= 3.14159f;
+        System.out.printf("Your value is %f",pi);
+
+    }
+}
